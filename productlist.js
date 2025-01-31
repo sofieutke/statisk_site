7e552422fd5e641673a59dae4cedabb07cf1cd2a
sofieutke <sobo0001@stud.kea.dk>
2025-01-31 11:24:29 +0100

    dynamisk list view

---
+++ productlist.js
@@ -0,0 +1,29 @@
+const product_list_container = document.querySelector(".product_list_container");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+
+function showList(products) {
+  let markup = "";
+  products
+    .map((product) => {
+      markup += `
+         <article class="product_card">
+                <a href="product.html">
+                    <div class="product_image">
+                        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}">
+                    </div>
+                    <div class="product_info">
+                        <h2>${product.productdisplayname}</h2>
+                        <p class="category">${product.articletype}</p>
+                        <p class="price">${product.price} DKK</p>
+                        <p class="read_more">Read More</p>
+                    </div>
+                </a>
+        </article>
+    `;
+    })
+    .join("");
+  product_list_container.innerHTML = markup;
+}
